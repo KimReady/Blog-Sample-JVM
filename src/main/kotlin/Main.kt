34c716eee26706c1c145a91b0000069d9f8c5f0e
KimReady <kjb4380@gmail.com>
2020-04-17 21:27:18 +0900

    Compare kotlinx.serialization wit gson

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,27 @@
+import com.google.gson.Gson
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+
+@Serializable
+data class User (
+    val name: String,
+    val email: String,
+    val age: Int = 20
+)
+
 fun main() {
-    println("Hello, Ready Story!")
+    val jsonString = """
+            {
+                "name" : "Ready Kim",
+                "email" : "ready.kim@gmail.com"
+            }
+        """.trimIndent()
+
+    val user = Gson().fromJson(jsonString, User::class.java)
+
+    println(user)
+
+    val user2 = Json.parse(User.serializer(), jsonString)
+
+    println(user2)
 }
