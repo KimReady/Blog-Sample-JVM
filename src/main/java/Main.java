74595066b1350cd3d774f989f2c5bbab9e5d7327
KimReady <kjb4380@gmail.com>
2020-05-05 23:17:24 +0900

    Add samples about 'is'

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
+
+class JavaA {
+    int a = 5;
+}
+
+class JavaB extends JavaA {
+    int b = 10;
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Ready Story!");
+        JavaA obj = new JavaB();
+        if (obj instanceof JavaB) {
+            JavaB obj2 = (JavaB) obj;
+            System.out.println(obj2.b);
+        }
     }
 }
